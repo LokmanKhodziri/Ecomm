b0572d53d8f81f26dba3f76f353902fa3188d086
lokmankhodziri <lokmankhodziri@gmail.com>
2025-05-12 13:54:38 +0800

    styling page and some fix

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import styles from "./page.module.css";
 import { stripe } from "@/lib/stripe";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import { Carousel } from "@/components/carousel";
 
 export default async function Home() {
 
@@ -13,17 +14,32 @@ const products = await stripe.products.list({
   
   return (
     <div>
-      <section>
-        <div>
-          <div>
-            <h2>Welcome to our store</h2>
-            <p>Discover new upgrade for your phone.</p>
-            <Button asChild variant="default">
-              <Link href="/products">Browse The Smartphone</Link>
+      <section className="rounded bg-neutral-100 py-8 sm:py-12">
+        <div className="mx-auto grid grid-clos-1 items-center justify-items-cnter gap-8 px-8 sm:px-16 md:grid-cols-2">
+          <div className="mx-w-md space-y-4">
+            <h2 className="text=3xl font-bold tracking-tight md-text-4xl">Welcome to our store</h2>
+            <p className="text-natural-600">Discover new upgrade for your phone.</p>
+            <Button 
+              asChild 
+              variant="default"
+              className="inline-flex items-center justify-center rounded-full px-6 py-3 bg-black text-white hover:bg-black/80"
+            >
+              <Link 
+              href="/products"
+              className="inline-flex items-center justify-center rounded-full px-6 py-3"
+              >Browse The Smartphone</Link>
             </Button>
           </div>
-          <Image alt="Banner Image" src={products.data[0].images[0]} width={500} height={500}/>
+          <Image 
+            alt="Banner Image" 
+            src={products.data[0].images[0]} 
+            className="rounded"
+            width={450} 
+            height={450}/>
         </div>
+      </section>
+      <section className="py-8">
+        <Carousel products={products.data} />
       </section>
     </div>
   );
---
+++ components/carousel.tsx
@@ -0,0 +1,47 @@
+'use client'
+
+import Stripe from "stripe"
+import { Card, CardContent, CardTitle } from "./ui/card"
+import { useState, useEffect } from "react"
+import Image from "next/image"
+
+interface Props {
+    products: Stripe.Product[];
+}
+
+export const Carousel = ({ products }: Props) => {
+    const [current, setCurrent] = useState<number>(0);
+
+    useEffect(() => {
+        if (!products?.length) return;
+        
+        const interval = setInterval(() => {
+            setCurrent((prev) => (prev + 1) % products.length);
+        }, 3000);
+
+        return () => clearInterval(interval);
+    }, [products?.length]);
+
+    if (!products?.length) return null;
+
+    const currentProduct = products[current];
+    const price = currentProduct.default_price as Stripe.Price;
+
+    return <Card className="relative overflow-hidden ronded-lf shadow-md border-gray-300">{currentProduct.images && currentProduct.images[0] && (
+        <div className="relative h-150 w-auto"><Image 
+            src={currentProduct.images[0]} 
+            alt={currentProduct.name}
+            fill
+            objectFit="cover"
+            className="transition-opacity duration-500 ease-in-out"
+        />
+        <CardContent className="absolute inset-0 flex flex-col items-center justify-center bg-black bg-opacity-50">
+            <CardTitle className="text-3xl font-bold text-white mb-2">{currentProduct.name}</CardTitle>
+                {price && price.unit_amount && 
+                    (<p className="text-white text-xl font-bold">
+                        RM{(price.unit_amount / 100).toFixed(2)}
+                    </p>)}
+        </CardContent>
+        </div>
+    )}</Card>;
+}
